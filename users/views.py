eb094119a4a4ffcf4db732e1dcb4a52bcd75b80d
evgeniia <jane87.05@mail.ru>
2023-12-09 20:45:10 +0300

    user

--- users/views.py
+++ users/views.py
@@ -3,8 +3,31 @@ from django.shortcuts import render
 from django.contrib.auth import login
 from django.contrib.auth.tokens import default_token_generator
 from django.contrib.auth.views import LoginView as BaseLoginView, PasswordResetDoneView
+from rest_framework import generics
 
 from users.models import User
+from users.serializers import UserSerializer
 
-class LoginView(BaseLoginView):
-    template_name = 'users/login.html'
+
+class UserCreateAPIView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserListAPIView(generics.ListAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+class UserRetrieveAPIView(generics.RetrieveAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+class UserUpdateAPIView(generics.UpdateAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+class UserDestroyAPIView(generics.DestroyAPIView):
+    queryset = User.objects.all()
