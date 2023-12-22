d49bb8fce6c2f6909c344229f0a5b949ca4990e6
evgeniia lemanova <jane87.05@mail.ru>
2023-12-22 17:40:05 +0300

    stripe

--- payments/views.py
+++ payments/views.py
@@ -1,13 +1,26 @@
+from django.db.migrations import serializer
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets, generics
 from rest_framework.filters import OrderingFilter
 
 from payments.models import Payment
 from payments.serializers import PaymentSerializer
+import stripe
 
 
 class PaymentCreateAPIView(generics.CreateAPIView):
     serializer_class = PaymentSerializer
+
+    def paument_create(self, serializer):
+        payment = serializer.save()
+        stripe.api_key = "pk_test_51OQ5WhB6xPKrOFgF4h2gDruKY4e99ratCgYbkh9IUc75yL4WrNzsVpEpbUHaTGJVnrzxegWwt4stS9Y9aAZzuBmE00WaW6nLh0"
+        pay = stripe.PaymentIntent.create(
+            amount=payment.amount,
+            currency='usd',
+            automatic_payment_methods={'enabled': True},)
+        pay.save()
+        return super().perform_create(serializer)
+
 
 class PaymentListAPIView(generics.ListAPIView):
     serializer_class = PaymentSerializer
